c5a0e0fbedb9ea1fe197a37c3f858307d03560cc
Jesse Luoto <jesse.luoto@gmail.com>
2021-06-24 22:32:33 +0300

    Add start page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,24 @@
+import Head from "next/head";
 import styles from "../styles/Header.module.scss";
 
-export default function Header({ buttons }: { buttons?: JSX.Element }) {
+export default function Header({
+  buttons,
+  additionalTitle,
+  description,
+}: {
+  buttons?: JSX.Element;
+  additionalTitle?: string;
+  description?: string;
+}) {
   return (
     <>
+      <Head>
+        <title>
+          kryptovero.fi{additionalTitle ? " | ".concat(additionalTitle) : ""}
+        </title>
+        {description && <meta name="description" content={description} />}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <header className={styles.header}>
         <div className={styles.logo}>kryptovero.fi</div>
         <div className={styles.buttons}>{buttons}</div>
---
+++ app/pages/start.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import Header from "../components/Header";
+import startIllustration from "../public/start-illustration.svg";
+import s from "../styles/Start.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Header additionalTitle="Aloita käyttö" />
+      <main className={s.centeredContainer}>
+        <Image src={startIllustration} alt="" />
+        <Link href="/onboarding">
+          <a className="btn">Olen ensikertalainen</a>
+        </Link>
+        <Link href="/load-file">
+          <a className="btn btn--secondary">Jatka siitä mihin jäin</a>
+        </Link>
+      </main>
+    </>
+  );
+}
