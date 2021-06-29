23c4d64f7d8344239864312a32a0a556da04c7b3
Jesse Luoto <jesse.luoto@gmail.com>
2021-06-29 21:53:38 +0300

    Add functionality to importers

--- app/pages/coinbase-import.tsx
+++ app/pages/coinbase-import.tsx
@@ -4,14 +4,28 @@ import ImporterDescription from "../components/ImporterDescription";
 import s from "../styles/CoinbaseImport.module.scss";
 import os from "../styles/Onboarding.module.scss";
 import Link from "next/link";
+import { useAppState } from "../components/app-state";
+import { useRouter } from "next/router";
 
 export default function CoinbaseImport() {
+  const addAppStateItem = useAppState();
+  const router = useRouter();
+
   return (
     <>
       <Header additionalTitle="Import from Coinbase"></Header>
       <main className={os.centeredContainer}>
         <h1>Tuo Coinbasesta...</h1>
-        <Importer>
+        <Importer
+          accept="text/csv"
+          onRead={async (file) => {
+            addAppStateItem({
+              type: "importCoinbaseCsv",
+              data: await file.text(),
+            });
+            router.push("/app");
+          }}
+        >
           Hae Coinbasesta<em>*</em> tekemäsi kaupat
           <ImporterDescription />
         </Importer>
--- app/pages/onboarding.tsx
+++ app/pages/onboarding.tsx
@@ -1,16 +1,30 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useAppState } from "../components/app-state";
 import Header from "../components/Header";
 import Importer from "../components/Importer";
 import ImporterDescription from "../components/ImporterDescription";
 import s from "../styles/Onboarding.module.scss";
 
 export default function Onboarding() {
+  const addAppStateItem = useAppState();
+  const router = useRouter();
+
   return (
     <>
       <Header additionalTitle="Tervetuloa" />
       <main className={s.centeredContainer}>
         <h1>Tervetuloa</h1>
-        <Importer>
+        <Importer
+          accept="text/csv"
+          onRead={async (file) => {
+            addAppStateItem({
+              type: "importCoinbaseCsv",
+              data: await file.text(),
+            });
+            router.push("/app");
+          }}
+        >
           Aloitetaan hakemalla Coinbasesta<em>*</em> tekemäsi kaupat
           <ImporterDescription />
         </Importer>
