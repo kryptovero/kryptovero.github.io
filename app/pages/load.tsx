4624d3685c8f692d15a3a940c763f2773689976e
Jesse Luoto <jesse.luoto@gmail.com>
2021-06-29 14:14:08 +0300

    Add loading data from a saved file

--- app/pages/load.tsx
+++ app/pages/load.tsx
@@ -3,14 +3,27 @@ import Importer from "../components/Importer";
 import s from "../styles/Load.module.scss";
 import os from "../styles/Onboarding.module.scss";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useAtom } from "jotai";
+import { appStateAtom } from "../components/app-state";
 
 export default function CoinbaseImport() {
+  const [_appState, setAppState] = useAtom(appStateAtom);
+  const router = useRouter();
+
   return (
     <>
       <Header additionalTitle="Import from Coinbase"></Header>
       <main className={`${os.centeredContainer} ${s.container}`}>
         <h1>Tervetuloa takaisin!</h1>
-        <Importer accept="application/kryptovero.fi">
+        <Importer
+          accept="application/kryptovero.fi"
+          onRead={async (file) => {
+            // Todo: Handle autosave
+            setAppState(JSON.parse(await file.text()));
+            router.push("/app");
+          }}
+        >
           <img src="/load.svg" alt="" />
           <p>
             Tiputa <em>.vero</em> -tiedosto tähän tai paina valitaksesi tiedosto{" "}
