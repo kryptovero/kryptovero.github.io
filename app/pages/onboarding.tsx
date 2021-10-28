40acf989fef0a4ae4cb08804c0249a4032ea9ade
Aleksi Huotala <aleksi.huotala@reaktor.com>
2021-10-28 07:32:26 +0300

    Initial UI for importing cryptocurrency transactions from multiple locations

--- app/pages/onboarding.tsx
+++ app/pages/onboarding.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import classNames from "classnames";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import Header from "../components/Header";
 import Importer from "../components/Importer";
 import ImporterDescription from "../components/ImporterDescription";
@@ -9,31 +11,66 @@ import s from "../styles/Onboarding.module.scss";
 export default function Onboarding() {
   const dispatch = useAppDispatch();
   const router = useRouter();
+  const [importLocation, setImportLocation] = useState<
+    "" | "coinbase" | "coinbasePro"
+  >("");
 
   return (
     <>
       <Header additionalTitle="Tervetuloa" />
       <main className={s.centeredContainer}>
         <h1>Tervetuloa</h1>
-        <Importer
-          accept="text/csv"
-          onRead={async (file) => {
-            dispatch(
-              insertEvent({
-                type: "importCoinbaseCsv",
-                data: await file.text(),
-                prefilledEurValues: {},
-              })
-            );
-            router.push("/app");
-          }}
-        >
-          Aloitetaan hakemalla Coinbase Pro:sta<em>*</em> tekemäsi kaupat
-          <ImporterDescription />
-        </Importer>
+        <div className={s.importLocationContainer}>
+          <h2>
+            Valitse ensin kauppapaikka, josta haluat hakea tekemäsi kaupat
+          </h2>
+          <div className={s.importLocationList}>
+            <div
+              onClick={(e) => {
+                setImportLocation("coinbase");
+              }}
+              className={classNames(s.importLocation, {
+                [s.disabledLocation]:
+                  importLocation !== "" && importLocation !== "coinbase",
+              })}
+            >
+              <img src="/coinbase.svg" alt="Coinbase" width={200} />
+            </div>
+            <div
+              onClick={(e) => {
+                setImportLocation("coinbasePro");
+              }}
+              className={classNames(s.importLocation, {
+                [s.disabledLocation]:
+                  importLocation !== "" && importLocation !== "coinbasePro",
+              })}
+            >
+              <img src="/coinbase.svg" alt="Coinbase" width={200} />
+              <strong>&nbsp;&nbsp;Pro</strong>
+            </div>
+          </div>
+        </div>
+        {importLocation !== "" && (
+          <Importer
+            accept="text/csv"
+            onRead={async (file) => {
+              dispatch(
+                insertEvent({
+                  type: "importCoinbaseCsv",
+                  data: await file.text(),
+                  prefilledEurValues: {},
+                })
+              );
+              router.push("/app");
+            }}
+          >
+            Aloitetaan hakemalla Coinbase Pro:sta<em>*</em> tekemäsi kaupat
+            <ImporterDescription />
+          </Importer>
+        )}
         <Link href="/app">
           <a className="btn btn--secondary">
-            En käytä Coinbase Pro:ta tai
+            En käytä Coinbase:a, Coinbase Pro:ta tai
             <br />
             haluan syöttää datan käsin
           </a>
